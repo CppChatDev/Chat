ecbcd02b1112ac4878890d549bb81902446d8133
Marius Brandt <m.brandt@app-arena.com>
2017-01-29 14:23:44 +0100

    Added simple username check and deny connection to unregistered users

--- Chat-Server/Authenticator.cpp
+++ Chat-Server/Authenticator.cpp
@@ -12,14 +12,33 @@ Authenticator::Authenticator(tcp::socket socket, Dispatcher& dispatcher) :
 void Authenticator::authenticate(std::function<void(std::shared_ptr<Session>)> on_success)
 {
 	auto self(shared_from_this());
+
 	socket.async_read_some(boost::asio::buffer(buffer, 1024),
 		[this, on_success, self](boost::system::error_code ec, size_t length)
 	{
 		// TODO
 		// authenticate
-
-		// for now - only read username
 		buffer[length] = 0;
-		on_success(std::make_shared<Session>(std::move(socket), dispatcher, buffer));
+		bool isAuthenticated = false;
+		auto users = database.execute("SELECT username FROM users");
+		for (auto&& user : users)
+		{
+			for (auto&& row : user)
+			{
+				if (row.second == buffer)
+				{
+					isAuthenticated = true;
+				}
+			}
+		}
+		
+		if (isAuthenticated)
+		{
+			on_success(std::make_shared<Session>(std::move(socket), dispatcher, buffer));
+		}
+		else
+		{
+			socket.write_some(boost::asio::buffer("Failed to authenticate: Username not registered"));
+		}
 	});
 }
